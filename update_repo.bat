5103d7c5b0f5bbf461783e810c75802a6456b446
jpascher <johann.pascher@gmail.com>
2025-04-20 08:30:20 +0200

    Neue Dateien hinzugef├╝gt und ├änderungen ├╝bernommen

--- update_repo.bat
+++ update_repo.bat
@@ -2,6 +2,11 @@
 cd /d %~dp0
 git status
 git add .
-git commit -m "Neue Dateien hinzugefügt und Änderungen übernommen"
-git push
+git diff --cached --quiet
+if %errorlevel%==1 (
+    git commit -m "Neue Dateien hinzugefügt und Änderungen übernommen"
+    git push
+) else (
+    echo Keine Änderungen zum Commit vorhanden.
+)
 pause
